5febd167c495ae482d05afaa2da9142d2f245194
RujiraKongsomran <rujirabiwberry@gmail.com>
2022-08-14 13:41:00 +0700

    Default Brushes

--- app/src/main/java/com/rujirakongsomran/jc_brushingupontext/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/jc_brushingupontext/MainActivity.kt
@@ -3,13 +3,24 @@ package com.rujirakongsomran.jc_brushingupontext
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.text.ExperimentalTextApi
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.rujirakongsomran.jc_brushingupontext.ui.theme.GradientColor1
+import com.rujirakongsomran.jc_brushingupontext.ui.theme.GradientColor2
+import com.rujirakongsomran.jc_brushingupontext.ui.theme.GradientColor3
 import com.rujirakongsomran.jc_brushingupontext.ui.theme.JC_BrushingUpOnTextTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,22 +33,89 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
                 }
             }
         }
     }
 }
 
+
+@OptIn(ExperimentalTextApi::class)
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun CreateDefaultBrushes() {
+    val gradientColors = listOf(GradientColor1, GradientColor2, GradientColor3)
+
+    Column(
+        modifier = Modifier
+            .padding(8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        Text(
+            text = "Do not allow what you cannot do,\n" +
+                    "Interfere with what you can do.\n" +
+                    "You can reach your dreams faster\n" +
+                    "Doing things you're good at.",
+            style = TextStyle(
+                brush = Brush.linearGradient(
+                    colors = gradientColors
+                )
+            ),
+        )
+        Text(
+            text = "Do not allow what you cannot do,\n" +
+                    "Interfere with what you can do.\n" +
+                    "You can reach your dreams faster\n" +
+                    "Doing things you're good at.",
+            style = TextStyle(
+                brush = Brush.horizontalGradient(
+                    colors = gradientColors
+                )
+            )
+        )
+
+        Text(
+            text = "Do not allow what you cannot do,\n" +
+                    "Interfere with what you can do.\n" +
+                    "You can reach your dreams faster\n" +
+                    "Doing things you're good at.",
+            style = TextStyle(
+                brush = Brush.verticalGradient(
+                    colors = gradientColors
+                )
+            )
+        )
+
+        Text(
+            text = "Do not allow what you cannot do,\n" +
+                    "Interfere with what you can do.\n" +
+                    "You can reach your dreams faster\n" +
+                    "Doing things you're good at.",
+            style = TextStyle(
+                brush = Brush.radialGradient(
+                    colors = gradientColors
+                )
+            )
+        )
+
+        Text(
+            text = "Do not allow what you cannot do,\n" +
+                    "Interfere with what you can do.\n" +
+                    "You can reach your dreams faster\n" +
+                    "Doing things you're good at.",
+            style = TextStyle(
+                brush = Brush.sweepGradient(
+                    colors = gradientColors
+                )
+            )
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     JC_BrushingUpOnTextTheme {
-        Greeting("Android")
+        CreateDefaultBrushes()
     }
 }
